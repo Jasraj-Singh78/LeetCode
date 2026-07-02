d09cd49a5f68ca04648f1ba242b744ad5d70b594
Jasraj Singh <166296031+Jasraj-Singh78@users.noreply.github.com>
2026-07-02 14:51:07 +0530

    Time: 0 ms (100%), Space: 47.1 MB (91.4%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,12 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        
-        
-        int xor=nums.length;
+        int xor=nums.length;                       //Approach 2 is using formula n=n*(n+1)/2 formula then compute the original array
+                                                    // then subtract value of n - nums array;
         for(int i=0;i<nums.length;i++){
               xor^=i;
-              xor^=nums[i];  
-
+              xor^=nums[i]; 
+              //xor=3(nums.length because we are computer it with that ) 
+        //Concept xor = 3 ^ 0 = 3
+               // xor = 3 ^ 3 = 0
             }
                return xor;
         }
