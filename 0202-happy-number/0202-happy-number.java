761ae51517f264cfefe2bab6d2125452f95d3ff2
Jasraj Singh <166296031+Jasraj-Singh78@users.noreply.github.com>
2026-08-07 05:29:48 +0530

    Time: 0 ms (100%), Space: 42.1 MB (95.32%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isHappy(int n) {
-    while(n!=1 && n!=4){
+    while(n!=1 && n!=4){ //Since 4 is part of that cycle, every unhappy number must pass through 4.
         int sum = 0;
         while(n!=0){
             int r = n%10;
