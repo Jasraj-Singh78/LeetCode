d0bf8fd60c943545df64faf55c8ab017c9f7b67c
Jasraj Singh <166296031+Jasraj-Singh78@users.noreply.github.com>
2026-08-21 06:50:18 +0530

    Time: 47 ms (14.57%), Space: 48.1 MB (23.36%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,7 +1,6 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        //max heap in pairs
-        PriorityQueue<int[]>mh=new PriorityQueue<>(
+         PriorityQueue<int[]>mh=new PriorityQueue<>(
             (a,b)->{
                 if(a[0]!=b[0])return b[0]-a[0]; //maximum distance
                 return b[1]-a[1]; //target value
@@ -9,17 +8,15 @@ class Solution {
         );
         int n=arr.length;
         for(int i=0;i<n;i++){
-            int dis=Math.abs(arr[i]-x);
+            int dis = Math.abs(arr[i]-x);
             mh.add(new int[]{dis,arr[i]});
             if(mh.size()>k)mh.poll();
         }
-        List<Integer>li = new ArrayList<>();
+        List<Integer>li=new ArrayList<>();
         while(!mh.isEmpty()){
             li.add(mh.poll()[1]);
         }
         Collections.sort(li);
         return li;
-
-
     }
 }
