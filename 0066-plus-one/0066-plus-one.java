9f420d16218bdf3b7e75a8ab3ad405a337e189af
Jasraj Singh <166296031+Jasraj-Singh78@users.noreply.github.com>
2026-08-23 13:00:39 +0530

    Time: 0 ms (100%), Space: 42.8 MB (98.73%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,19 +1,14 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-
-        for (int i = digits.length - 1; i >= 0; i--) {
-
-            if (digits[i] < 9) {
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i]<9){
                 digits[i]++;
                 return digits;
             }
-
-            digits[i] = 0;
+            digits[i]=0;
         }
-
-        int[] ans = new int[digits.length + 1];
-        ans[0] = 1;
-
+        int[] ans=new int[digits.length+1]; //if array is 999 we need addition space for 1 to get 1000
+        ans[0]=1;     //not return digits just return ans as 1,0,0,0 ;
         return ans;
     }
 }
