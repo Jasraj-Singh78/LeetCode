30883b209a2228da175f0902e16fa40ef1eada36
Jasraj Singh <166296031+Jasraj-Singh78@users.noreply.github.com>
2026-08-24 15:10:35 +0530

    Time: 14 ms (70.82%), Space: 47.7 MB (33.81%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -4,22 +4,55 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        PriorityQueue<Integer>minh=new PriorityQueue<>(
-            (a,b)->map.get(a)-map.get(b)
+                PriorityQueue<int []>minh=new PriorityQueue<>(
+            (a,b)->a[0] - b[0]
         );
-        for(int num : map.keySet()){
-            minh.add(num);
-        
-        while(minh.size()>k){
-            minh.poll();
-    }
-    
-    }
-    int[] ans=new int[k];
+        for(Map.Entry<Integer,Integer> ent:map.entrySet()){
+            int element = ent.getKey();
+            int freq = ent.getValue();
+            minh.add(new int[]{freq,element});
+            if(minh.size()>k)minh.poll();
+        }
+         int[] ans=new int[k];
      for(int i=0;i<k;i++){
-        ans[i]=minh.poll();
+        ans[i]=minh.poll()[1]; //need to get element from 1st index not frequency on 0th
      }   
      return ans;
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+//         PriorityQueue<Integer>minh=new PriorityQueue<>(
+//             (a,b)->map.get(a)-map.get(b)
+//         );
+//         for(int num : map.keySet()){
+//             minh.add(num);
+        
+//         while(minh.size()>k){
+//             minh.poll();
+//     }
+    
+//     }
+//     int[] ans=new int[k];
+//      for(int i=0;i<k;i++){
+//         ans[i]=minh.poll();
+//      }   
+//      return ans;
+//     }
+
+// }
