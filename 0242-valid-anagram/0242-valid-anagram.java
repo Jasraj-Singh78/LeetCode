11c834e8a8024060cb8785690337828e0b3223fb
Jasraj Singh <166296031+Jasraj-Singh78@users.noreply.github.com>
2026-09-21 14:51:56 +0530

    Time: 17 ms (16.56%), Space: 46.6 MB (15.8%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,18 +4,18 @@ class Solution {
       if(s.length()!=t.length())return false;
       for(int i=0;i<s.length();i++){
         char ch = s.charAt(i);
-        map.put(ch,map.getOrDefault(ch,0)+1);
+        map.put(ch,map.getOrDefault(ch,0)+1);// add into map for string s
       }
-      for(int i=0;i<t.length();i++){
+      for(int i=0;i<t.length();i++){ 
         char ch = t.charAt(i);
-        if(!map.containsKey(ch))return false;
-        map.put(ch,map.get(ch)-1);
-        if(map.get(ch)==0){
+        if(!map.containsKey(ch))return false; //if key mismatch obv false
+        map.put(ch,map.get(ch)-1);   //if key matched subtract it 
+        if(map.get(ch)==0){             //if key value becomes zero remove it
             map.remove(ch);
         }
             
         }
-        return map.isEmpty();
+        return map.isEmpty();  //after removing value if map is empty return empty map as the strings are anagram
       }
       
         
