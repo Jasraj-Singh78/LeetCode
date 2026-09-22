9487a6c297fba3ab308527c7920a20976d21a74d
Jasraj Singh <166296031+Jasraj-Singh78@users.noreply.github.com>
2026-09-22 10:44:02 +0530

    Time: 24 ms (19.26%), Space: 43.6 MB (86.22%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -6,22 +6,14 @@ class Solution {
        for(int i=0;i<n;i++){
         char ch1 = s.charAt(i);
         char ch2 = t.charAt(i);
-        if(map1.containsKey(ch1)){
-            if(map1.get(ch1)!=ch2)
-        return false;
-        }
-        else{
-            map1.put(ch1,ch2);
-        }
-        if(map2.containsKey(ch2)){
-            if(map2.get(ch2)!=ch1)
+        if(map1.containsKey(ch1)&& map1.get(ch1)!=ch2)
             return false;
-        }
         
-
-        else{
-            map2.put(ch2,ch1);
-        }
+        if(map2.containsKey(ch2)&& map2.get(ch2)!=ch1)
+            return false;
+        
+        map1.put(ch1,ch2);
+        map2.put(ch2,ch1);
        } 
        return true;
     }
