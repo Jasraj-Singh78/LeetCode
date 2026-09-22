16c83dbe90a74c5c9420a3553a04e9cc424c0119
Jasraj Singh <166296031+Jasraj-Singh78@users.noreply.github.com>
2026-09-22 12:37:38 +0530

    Time: 6 ms (98.99%), Space: 49.4 MB (92.07%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,19 +1,13 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String,List<String>> mp = new HashMap<>();
-        for(String word : strs){
-            //convert string into character
-            char ch[]=word.toCharArray();
-            //sort on key bases
+        Map<String,List<String>> map = new HashMap<>();
+        for(String word:strs){
+            char [] ch = word.toCharArray();
             Arrays.sort(ch);
-            // create key
             String key = new String(ch);
-            //Add string to corresponding group
-            mp.putIfAbsent(key, new ArrayList<>());
-            mp.get(key).add(word);
-
+            map.putIfAbsent(key,new ArrayList<>());
+            map.get(key).add(word);
         }
-        return new ArrayList<>(mp.values()); //why again arraylist beacuse we need to return array list of list this will
-                                                //return in list of list
+        return new ArrayList<>(map.values());
     }
 }
